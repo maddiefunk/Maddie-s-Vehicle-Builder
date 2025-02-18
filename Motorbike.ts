e19242b3e4ccbcb00e98301a5f40ed86bad7729c
Madeline Funk <madeline.funk91@gmail.com>
2025-02-18 15:55:48 -0600

    Add files via upload

---
+++ Motorbike.ts
@@ -0,0 +1,66 @@
+// Importing Vehicle and Wheel classes
+import Vehicle from './Vehicle.js';
+import Wheel from './Wheel.js';
+
+class Motorbike extends Vehicle {
+
+  vin: string;
+  color: string;
+  make: string; 
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+
+  constructor (
+  vin: string,
+  color: string,
+  make: string,
+  model: string,
+  year: number,
+  weight: number,
+  topSpeed: number,
+  wheels: Wheel[],
+  ) {
+    super();
+    this.vin = vin;
+    this.color = color;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.weight = weight;
+    this.topSpeed = topSpeed;
+    this.wheels = wheels;
+
+    if (wheels.length !==2) {
+      this.wheels = [new Wheel(), new Wheel()];
+    } else {
+      this.wheels = wheels;
+    }
+  }
+    wheelie(): void {
+      console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+    }
+  
+  // TODO: Override the printDetails method from the Vehicle class
+  override printDetails(): void {
+    super.printDetails();
+    console.log(`Vin: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`);
+    console.log(`Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`);
+
+  }
+  // TODO: The method should call the printDetails method of the parent class
+  // TODO: The method should log the details of the Motorbike
+  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+  }
+
+// Export the Motorbike class as the default export
+export default Motorbike;
